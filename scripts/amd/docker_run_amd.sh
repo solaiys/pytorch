078226a3651c7615d3223ae1deeba03a50651554
Michael Melesse <micmelesse@gmail.com>
2021-02-23 13:07:20 -0800

    update scripts

--- scripts/amd/docker_run_amd.sh
+++ scripts/amd/docker_run_amd.sh
@@ -1,9 +1,14 @@
 alias drun='sudo docker run -it --rm --network=host --device=/dev/kfd --device=/dev/dri --ipc=host --shm-size 16G --group-add video --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -v $HOME/dockerx:/dockerx -v /data:/data -v /nfs_megatron:/nfs_megatron'
 alias drun_nodevice='sudo docker run -it --rm --network=host --ipc=host --shm-size 16G --group-add video --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -v $HOME/dockerx:/dockerx -v /data:/data -v /nfs_megatron:/nfs_megatron'
 
-
 # WORK_DIR=/var/lib/jenkins/pytorch
-WORK_DIR='/dockerx/pytorch'
+# WORK_DIR='/dockerx/pytorch'
+WORK_DIR='/root/pytorch'
 
-drun -w $WORK_DIR rocm/pytorch:rocm4.0_ubuntu18.04_py3.6_pytorch
-# drun -w $WORK_DIR rocm/pytorch-private:rocm4.0_ubuntu18.04_py3.6_pytorch_rocfft
+# IMAGE_NAME=rocm/pytorch
+# IMAGE_NAME=rocm/pytorch:rocm4.0_ubuntu18.04_py3.6_pytorch
+IMAGE_NAME=rocm/pytorch:rocm4.0.1_ubuntu18.04_py3.6_pytorch
+
+drun -d --name pytorch_container -w $WORK_DIR $IMAGE_NAME
+# docker cp . pytorch_container:/root/pytorch
+docker attach pytorch_container
--- scripts/amd/run.sh
+++ scripts/amd/run.sh
@@ -1,3 +1,4 @@
-# sh scripts/amd/install_pytorch_develop.sh
+set -e
+
 sh scripts/amd/jenkins_pytorch_build.sh
-sh scripts/amd/test_spectral.sh
+# sh scripts/amd/install_pytorch_develop.sh
