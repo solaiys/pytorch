9443f0aff22ae5d9ade0d96fe85d8e6bb9abe243
Michael Melesse <micmelesse@gmail.com>
2021-03-03 15:01:25 -0800

    update scripts

--- scripts/amd/docker_run_amd.sh
+++ scripts/amd/docker_run_amd.sh
@@ -1,5 +1,7 @@
-alias drun='sudo docker run -it --rm --network=host --device=/dev/kfd --device=/dev/dri --ipc=host --shm-size 16G --group-add video --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -v $HOME/dockerx:/dockerx -v /data:/data -v /nfs_megatron:/nfs_megatron'
-alias drun_nodevice='sudo docker run -it --rm --network=host --ipc=host --shm-size 16G --group-add video --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -v $HOME/dockerx:/dockerx -v /data:/data -v /nfs_megatron:/nfs_megatron'
+alias drun='sudo docker run -it --rm --network=host --device=/dev/kfd --device=/dev/dri --ipc=host --shm-size 16G --group-add video --cap-add=SYS_PTRACE --security-opt seccomp=unconfined'
+alias drun_nodevice='sudo docker run -it --rm --network=host --ipc=host --shm-size 16G --group-add video --cap-add=SYS_PTRACE --security-opt seccomp=unconfined'
+
+VOLUMES="-v $HOME/dockerx:/dockerx -v /data:/data"
 
 # WORK_DIR=/var/lib/jenkins/pytorch
 # WORK_DIR='/dockerx/pytorch'
@@ -9,6 +11,6 @@ WORK_DIR='/root/pytorch'
 # IMAGE_NAME=rocm/pytorch:rocm4.0_ubuntu18.04_py3.6_pytorch
 IMAGE_NAME=rocm/pytorch:rocm4.0.1_ubuntu18.04_py3.6_pytorch
 
-drun -d --name pytorch_container -w $WORK_DIR $IMAGE_NAME
-# docker cp . pytorch_container:/root/pytorch
+drun -d --name pytorch_container -w $WORK_DIR $VOLUMES $IMAGE_NAME
+docker cp . pytorch_container:/root/pytorch
 docker attach pytorch_container
